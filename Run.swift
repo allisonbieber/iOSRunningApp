7841480f8ceda23130a3f5422ac64bf80d74f690
Allison Bieber <bieber.a@husky.neu.edu>
2019-08-13 19:38:02 -0700

    Initial run class

--- Run.swift
+++ Run.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+/**
+ This class represents a single run object.
+ **/
+class Run {
+    
+    var distance: Double!
+    var time: Int!
+    var year: Int!
+    var month: Int!
+    var day: Int!
+    
+    // Constructor for a run object
+    init(distance: Double, time: Int, year: Int, month: Int, day: Int ) {
+        self.distance = distance
+        self.time = time
+        self.year = year
+        self.month = month
+        self.day = day
+    }
+    
+    func returnDate() {
+        
+    }
+}
