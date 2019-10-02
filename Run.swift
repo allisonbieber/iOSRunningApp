0516753f842caeda8d567ef7b9f368574c062584
Allison Bieber <bieber.a@husky.neu.edu>
2019-10-02 14:47:54 -0700

    Updates to Run class

--- Run.swift
+++ Run.swift
@@ -21,13 +21,26 @@ class Run {
     // Constructor for a run object
     init(distance: Double, time: Int, year: Int, month: Int, day: Int ) {
         self.distance = distance
-        self.time = time
+        self.time = time //time in seconds
         self.year = year
         self.month = month
         self.day = day
     }
     
-    func returnDate() {
+    func returnDate() -> String {
+        let date = String(self.day) + String(self.month) + String(self.year)
+        return date
+    }
+    
+    func getPace() -> Double{
+        let pace = 0.0
+        
+        let hours = self.time / 3600
+        let minutes = hours % 60
+        let seconds = minutes % 60
+        
+        
+        return pace
         
     }
 }
